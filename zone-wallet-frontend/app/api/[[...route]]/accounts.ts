262a4cef996794c1dddbfee5f58c3ca5a43fb830
Chukwudi Chidubem <chidubemchukwudi@gmail.com>
2025-04-19 03:14:50 +0100

    feat: implement wallets management with bulk delete functionality and UI components

--- zone-wallet-frontend/app/api/[[...route]]/accounts.ts
+++ zone-wallet-frontend/app/api/[[...route]]/accounts.ts
@@ -1,6 +1,7 @@
+import { z } from "zod";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
 
@@ -49,6 +50,38 @@ const app = new Hono()
           ...values,
         })
         .returning();
+
+      return c.json({ data });
+    }
+  )
+  .post(
+    "/bult-delete",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.object({
+        ids: z.array(z.string()),
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized " }, 401);
+      }
+
+      const data = await db
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            inArray(accounts.id, values.ids)
+          )
+        )
+        .returning({
+          id: accounts.id,
+        });
 
       return c.json({ data });
     }
--- zone-wallet-frontend/components/navigation.tsx
+++ zone-wallet-frontend/components/navigation.tsx
@@ -24,7 +24,7 @@ const routes = [
   },
   {
     href: "/accounts",
-    label: "Accounts",
+    label: "Wallets",
   },
   {
     href: "/settings",
