6993929ce5e2e18d9b66b65924145b65760890a3
Chukwudi Chidubem <chidubemchukwudi@gmail.com>
2025-06-06 00:41:04 +0100

    feat: refactor transaction actions and sheets for improved transaction management

---
+++ zone-wallet-frontend/features/transactions/components/edit-transaction-sheet.tsx
@@ -0,0 +1,126 @@
+import { z } from "zod";
+
+import { Loader2 } from "lucide-react";
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+import { useGetTransaction } from "../api/use-get-transaction";
+import { useOpenTransaction } from "../hooks/use-open-transaction";
+import { useEditTransaction } from "../api/use-edit-transaction";
+import { useDeleteTransaction } from "../api/use-delete-transaction";
+import { useConfirm } from "@/hooks/use-confirm";
+import { TransactionForm } from "./transaction-form";
+
+import { insertTransactionSchema } from "@/db/schema";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
+const formSchema = insertTransactionSchema.omit({
+  id: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+export const EditTransactionSheet = () => {
+  const { isOpen, onClose, id } = useOpenTransaction();
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this transaction."
+  );
+
+  const transactionQuery = useGetTransaction(id);
+  const editMutation = useEditTransaction(id);
+  const deleteMutation = useDeleteTransaction(id);
+
+  const accountQuery = useGetAccounts();
+  const accountMutation = useCreateAccount();
+  const onCreateAccount = (name: string) =>
+    accountMutation.mutate({
+      name,
+    });
+  const accountOptions = (accountQuery.data ?? []).map((account) => ({
+    label: account.name,
+    value: account.id,
+  }));
+
+  const isPending =
+    editMutation.isPending ||
+    deleteMutation.isPending ||
+    transactionQuery.isLoading ||
+    accountMutation.isPending;
+
+  const isLoading = transactionQuery.isLoading || accountQuery.isLoading;
+
+  const onSubmit = (values: FormValues) => {
+    editMutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  const onDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) {
+      deleteMutation.mutate(undefined, {
+        onSuccess: () => {
+          onClose();
+        },
+      });
+    }
+  };
+
+  const defaultValues = transactionQuery.data
+    ? {
+        accountId: transactionQuery.data.accountId,
+        amount: transactionQuery.data.amount.toString(),
+        date: transactionQuery.data.date
+          ? new Date(transactionQuery.data.date)
+          : new Date(),
+        payee: transactionQuery.data.payee,
+        notes: transactionQuery.data.notes,
+      }
+    : {
+        accountId: "",
+        amount: "",
+        date: new Date(),
+        payee: "",
+        notes: "",
+      };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent className="space-y-4">
+          <SheetHeader>
+            <SheetTitle>Edit Transaction</SheetTitle>
+            <SheetDescription>Edit an existing transaction.</SheetDescription>
+          </SheetHeader>
+          {isLoading ? (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Loader2 className="size-4 text-muted-foreground animate-spin" />
+            </div>
+          ) : (
+            <TransactionForm
+              id={id}
+              defaultValues={defaultValues}
+              onSubmit={onSubmit}
+              onDelete={onDelete}
+              disabled={isPending}
+              accountOptions={accountOptions}
+              onCreateAccount={onCreateAccount}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
--- zone-wallet-frontend/providers/sheet-provider.tsx
+++ zone-wallet-frontend/providers/sheet-provider.tsx
@@ -6,6 +6,7 @@ import { NewAccountSheet } from "@/features/accounts/components/new-account-shee
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 
 import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
 
 export const SheetProvider = () => {
     const isMounted = useMountedState()
@@ -17,6 +18,7 @@ export const SheetProvider = () => {
       <EditAccountSheet />
 
       <NewTransactionSheet/>
+      <EditTransactionSheet/>
     </>
   );
 };
