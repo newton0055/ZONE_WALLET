3f6787436033349455eb450298646a218b922140
Chukwudi Chidubem <chidubemchukwudi@gmail.com>
2025-06-18 15:06:53 +0100

    feat: add area chart and custom tooltip components, integrate with data charts

--- zone-wallet-frontend/app/api/[[...route]]/summary.ts
+++ zone-wallet-frontend/app/api/[[...route]]/summary.ts
@@ -100,7 +100,7 @@ const app = new Hono().get(
             Number
           ),
         debit:
-          sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+          sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ABS(${transactions.amount}) ELSE 0 END)`.mapWith(
             Number
           ),
       })
