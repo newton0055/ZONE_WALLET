1b4e4aec3df2abcba15314049b5aee06991c10f6
Chukwudi Chidubem <chidubemchukwudi@gmail.com>
2025-06-18 16:51:03 +0100

    feat: enhance loading state with skeleton and spinner in data charts

--- zone-wallet-frontend/components/data-charts.tsx
+++ zone-wallet-frontend/components/data-charts.tsx
@@ -2,24 +2,28 @@
 
 import { useGetSummary } from "@/features/summary/api/use-get-summary";
 
-import { Chart } from "./chart";
+import { Chart, ChartLoading } from "./chart";
 
 export const DataCharts = () => {
-const { data, isLoading } = useGetSummary();
+  const { data, isLoading } = useGetSummary();
 
-if (isLoading) {
+  if (isLoading) {
     return (
-        <div>
-            loading...
-        </div>
-    )
-}
-
-    return (
+      <div>
         <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
-            <div className="col-span-1 lg:col-span-3 xl:col-span-4">
-            <Chart data={data?.days}/>
-            </div>
+          <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+            <ChartLoading />
+          </div>
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        <Chart data={data?.days} />
+      </div>
+    </div>
+  );
 };
